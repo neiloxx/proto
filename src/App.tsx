4add3295a695b2e3f35ae997658e2f716d60d2e1
neiloxx <anastasia.v.sido@gmail.com>
2023-08-01 22:22:50 +0300

    feat: test react-div-100vh

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import photos from './mocks/mocks.json';
 import {PhotoCard} from "./components/PhotoCard/PhotoCard.tsx";
+import Div100vh from "react-div-100vh";
 
 function App() {
 
   return (
-    <>
+    <Div100vh>
       {photos.map(({id, url}) => (<PhotoCard key={id} url={url}/>))}
-    </>
+    </Div100vh>
   )
 }
 
